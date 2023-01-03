8899956cd9b73aa4a5fc2d5f15a7efa37f5761d5
Code Sandbox <hello@codesandbox.io>
2023-01-03 03:12:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.be04c88183b7164883169546326951f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ba5be1c6c912703ec4e6f29f02cc21",
+    "url": "https://ji-soft.github.io/csb-8ne6cf/index.html"
+  },
+  {
+    "revision": "213d70bb43058a9f5fa3",
+    "url": "https://ji-soft.github.io/csb-8ne6cf/static/css/main.7a369d86.chunk.css"
+  },
+  {
+    "revision": "2f23db5d7b27e8a78a82",
+    "url": "https://ji-soft.github.io/csb-8ne6cf/static/js/2.4228bcbc.chunk.js"
+  },
+  {
+    "revision": "213d70bb43058a9f5fa3",
+    "url": "https://ji-soft.github.io/csb-8ne6cf/static/js/main.e1e98151.chunk.js"
+  },
+  {
+    "revision": "0a0b4c14022d9b91a1ef",
+    "url": "https://ji-soft.github.io/csb-8ne6cf/static/js/runtime-main.0e41deba.js"
+  }
+]);
